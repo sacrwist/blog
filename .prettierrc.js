aa747f2b8e3932421f54c8e35b06c88b640592c9
sacrwist <sacrwist@gmail.com>
2025-04-02 00:06:12 +0900

    ➕ prettier-plugin-tailwindcss

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,9 +3,15 @@ module.exports = {
   semi: true, // 文末にセミコロンを付ける
   singleQuote: true, // シングルクォートを使用する (JSXを除く)
   jsxSingleQuote: false, // JSX ではダブルクォートを使用する
-  trailingComma: 'es5', // 末尾のカンマを ES5 互換の方法で追加 (オブジェクト、配列など)
-  printWidth: 80, // 1行の最大文字数
+  trailingComma:
+    'es5', // 末尾のカンマを ES5 互換の方法で追加 (オブジェクト、配列など)
+  printWidth: 20, // 1行の最大文字数
   tabWidth: 2, // タブの幅（スペース数）
   useTabs: false, // タブの代わりにスペースを使用する
-  arrowParens: 'always',
-};
+  arrowParens:
+    'always',
+  endOfLine: 'lf',
+  plugins: [
+    'prettier-plugin-tailwindcss',
+  ],
+}
