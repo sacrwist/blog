4d70574b8f421a93539d7e7f2a2c229004a0f1ff
sacrwist <sacrwist@gmail.com>
2025-04-02 20:18:29 +0900

    ✅ article component

---
+++ src/features/articles/components/ArticleCard.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const ArticleCard = () => {
+  return (
+    <article className="overflow-hidden rounded-lg shadow-lg transition-shadow duration-500 hover:shadow-2xl">
+      <Link href="">
+        <Image
+          alt=""
+          src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          className="h-56 w-full object-cover"
+          width={300}
+          height={300}
+        />
+
+        <div className="bg-white p-4 sm:p-6">
+          <time
+            // datetime="2022-10-10"
+            className="block text-xs text-gray-500"
+          >
+            {' '}
+            10th Oct 2022{' '}
+          </time>
+
+          <h3 className="mt-0.5 text-xl text-gray-900">
+            How to position your furniture for positivity
+          </h3>
+
+          <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+            dolores, possimus pariatur animi temporibus nesciunt praesentium
+            dolore sed nulla ipsum eveniet corporis quidem, mollitia itaque
+            minus soluta, voluptates neque explicabo tempora nisi culpa eius
+            atque dignissimos. Molestias explicabo corporis voluptatem?Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+            dolores, possimus pariatur animi temporibus nesciunt praesentium
+            dolore sed nulla ipsum eveniet corporis quidem, mollitia itaque
+            minus soluta, voluptates neque explicabo tempora nisi culpa eius
+            atque dignissimos. Molestias explicabo corporis voluptatem?Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+            dolores, possimus parusandae dolores, possimus parusandae dolores,
+            possimus parusandae dolores, possimus parusandae dolores, possimus
+            pariatur animi temporibus nesciunt praesentium dolore sed nulla
+            ipsum eveniet corporis quidem, mollitia itaque minus soluta,
+            voluptates neque explicabo tempora nisi culpa eius atque
+            dignissimos. Molestias explicabo corporis voluptatem?
+          </p>
+        </div>
+      </Link>
+    </article>
+  );
+};
