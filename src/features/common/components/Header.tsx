54939102712761e04ab7c762838b59d502ec9c96
sacrwist <sacrwist@gmail.com>
2025-04-02 13:05:53 +0900

    ✨ Header

--- src/features/common/components/Header.tsx
+++ src/features/common/components/Header.tsx
@@ -1,3 +1,23 @@
+import Link from 'next/link';
+
 export const Header = () => {
-  return <div className="text-2xl">Header</div>;
+  return (
+    <header className="flex flex-col items-center pt-8">
+      <h1 className="text-4xl">Title</h1>
+      <nav className="pt-4">
+        <ul className="flex gap-2">
+          <li>
+            <Link href="/" className="block p-2 text-xl">
+              Next
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="block p-2 text-xl">
+              Node
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
