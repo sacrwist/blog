38b220929972fbbb0728009f7bd5df9567e2da66
sacrwist <sacrwist@gmail.com>
2025-04-04 10:58:06 +0900

    ✅

--- src/features/common/components/Header.tsx
+++ src/features/common/components/Header.tsx
@@ -1,23 +1,56 @@
+import { Button } from '@/components/ui/button';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import Link from 'next/link';
 
 export const Header = () => {
+  const tabItems = [
+    { value: 'all', label: 'All' },
+    { value: 'design', label: 'Design' },
+    { value: 'technology', label: 'Technology' },
+    { value: 'travel', label: 'Travel' },
+  ];
+
   return (
-    <header className="flex flex-col items-center pt-8">
-      <h1 className="text-4xl">Title</h1>
-      <nav className="pt-4">
-        <ul className="flex gap-2">
-          <li>
-            <Link href="/" className="block p-2 text-xl">
-              Next
-            </Link>
-          </li>
-          <li>
-            <Link href="/" className="block p-2 text-xl">
-              Node
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <header className="sticky top-0 bg-white pb-8 pt-10 shadow-lg">
+      <div className="mx-auto max-w-7xl">
+        <div className="flex items-center justify-between">
+          <h1 className="text-4xl">
+            <Link href="/">Title</Link>
+          </h1>
+          <div className="">
+            <Button
+              variant="outline"
+              className="text-md border-black px-6 py-5"
+            >
+              Contact
+            </Button>
+          </div>
+        </div>
+
+        <Tabs defaultValue="all" className="w-full">
+          <TabsList className="inline-flex h-auto rounded-none border-none bg-transparent pt-10">
+            {tabItems.map((tab) => (
+              <TabsTrigger
+                key={tab.value}
+                value={tab.value}
+                className="relative h-auto rounded-none border-b-2 border-transparent bg-transparent px-4 pb-[calc(0.5rem+2px)] pt-2 font-semibold text-muted-foreground shadow-none transition-none hover:text-foreground focus-visible:ring-0 focus-visible:ring-offset-0 data-[state=active]:border-primary data-[state=active]:text-foreground data-[state=active]:shadow-none"
+              >
+                {tab.label}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+
+          {/* 各タブに対応するコンテンツエリア */}
+          {tabItems.map((tab) => (
+            <TabsContent key={tab.value} value={tab.value} className="mt-4">
+              {/* ここに各タブのコンテンツを表示します */}
+              <p className="p-4">
+                Content for <strong>{tab.label}</strong>
+              </p>
+            </TabsContent>
+          ))}
+        </Tabs>
+      </div>
     </header>
   );
 };
