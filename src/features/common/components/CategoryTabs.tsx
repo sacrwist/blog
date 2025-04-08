b52b7f23ad9f81f1e5c03ad79493c2e86058ec3b
sacrwist <sacrwist@gmail.com>
2025-04-09 01:54:19 +0900

    ✅

---
+++ src/features/common/components/CategoryTabs.tsx
@@ -0,0 +1,65 @@
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+interface CategoryTabsProps {
+  categories: {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    revisedAt: string;
+    name: string;
+    slug: string;
+  }[];
+}
+
+export const CategoryTabs = (props: CategoryTabsProps) => {
+  const pathname = usePathname();
+
+  // 現在アクティブにすべきタブの値を決定
+  let activeTabValue = 'all'; // デフォルトは 'all'
+  if (pathname !== '/') {
+    // パスからカテゴリ部分を取得 (例: "/design" -> "design")
+    const pathSegments = pathname.split('/');
+    if (
+      pathSegments.length > 1 &&
+      props.categories.some(
+        (category) =>
+          category.slug === pathSegments[1] && category.slug !== 'all'
+      )
+    ) {
+      activeTabValue = pathSegments[1];
+    } else {
+      // / 以外の不明なパスや、カテゴリに一致しない場合は 'all' (または非選択) のまま
+      activeTabValue = '';
+    }
+  }
+
+  return (
+    <Tabs value={activeTabValue} defaultValue="all" className="w-full">
+      <TabsList className="inline-flex h-auto rounded-none border-none bg-transparent pt-10">
+        <Link key="all" href="/">
+          <TabsTrigger
+            key="all"
+            value="all"
+            className="relative h-auto rounded-none border-b-2 border-transparent bg-transparent px-4 pb-[calc(0.5rem+2px)] pt-2 font-semibold text-muted-foreground shadow-none transition-none hover:text-foreground focus-visible:ring-0 focus-visible:ring-offset-0 data-[state=active]:border-primary data-[state=active]:text-foreground data-[state=active]:shadow-none"
+          >
+            All
+          </TabsTrigger>
+        </Link>
+        {props.categories.map((category) => (
+          <Link key={category.id} href={`/${category.slug}`}>
+            <TabsTrigger
+              key={category.id}
+              value={category.slug}
+              className="relative h-auto rounded-none border-b-2 border-transparent bg-transparent px-4 pb-[calc(0.5rem+2px)] pt-2 font-semibold text-muted-foreground shadow-none transition-none hover:text-foreground focus-visible:ring-0 focus-visible:ring-offset-0 data-[state=active]:border-primary data-[state=active]:text-foreground data-[state=active]:shadow-none"
+            >
+              {category.name}
+            </TabsTrigger>
+          </Link>
+        ))}
+      </TabsList>
+    </Tabs>
+  );
+};
--- src/features/common/components/Header.tsx
+++ src/features/common/components/Header.tsx
@@ -1,8 +1,7 @@
 'use client';
 import { Button } from '@/components/ui/button';
-import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { CategoryTabs } from '@/features/common/components/CategoryTabs';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 
 interface HeaderProps {
   categories: {
@@ -17,30 +16,8 @@ interface HeaderProps {
 }
 
 export const Header = (props: HeaderProps) => {
-  console.log(props.categories);
-  const pathname = usePathname();
-
-  // 現在アクティブにすべきタブの値を決定
-  let activeTabValue = 'all'; // デフォルトは 'all'
-  if (pathname !== '/') {
-    // パスからカテゴリ部分を取得 (例: "/design" -> "design")
-    const pathSegments = pathname.split('/');
-    if (
-      pathSegments.length > 1 &&
-      props.categories.some(
-        (category) =>
-          category.slug === pathSegments[1] && category.slug !== 'all'
-      )
-    ) {
-      activeTabValue = pathSegments[1];
-    } else {
-      // / 以外の不明なパスや、カテゴリに一致しない場合は 'all' (または非選択) のまま
-      activeTabValue = '';
-    }
-  }
-
   return (
-    <header className="sticky top-0 bg-white pb-8 pt-10 shadow-lg">
+    <header className="sticky top-0 z-50 bg-white pb-8 pt-10 shadow-lg">
       <div className="mx-auto max-w-7xl">
         <div className="flex items-center justify-between">
           <h1 className="text-4xl">
@@ -57,31 +34,7 @@ export const Header = (props: HeaderProps) => {
             </Link>
           </div>
         </div>
-
-        <Tabs value={activeTabValue} defaultValue="all" className="w-full">
-          <TabsList className="inline-flex h-auto rounded-none border-none bg-transparent pt-10">
-            <Link key="all" href="/">
-              <TabsTrigger
-                key="all"
-                value="all"
-                className="relative h-auto rounded-none border-b-2 border-transparent bg-transparent px-4 pb-[calc(0.5rem+2px)] pt-2 font-semibold text-muted-foreground shadow-none transition-none hover:text-foreground focus-visible:ring-0 focus-visible:ring-offset-0 data-[state=active]:border-primary data-[state=active]:text-foreground data-[state=active]:shadow-none"
-              >
-                All
-              </TabsTrigger>
-            </Link>
-            {props.categories.map((category) => (
-              <Link key={category.id} href={`/${category.slug}`}>
-                <TabsTrigger
-                  key={category.id}
-                  value={category.slug}
-                  className="relative h-auto rounded-none border-b-2 border-transparent bg-transparent px-4 pb-[calc(0.5rem+2px)] pt-2 font-semibold text-muted-foreground shadow-none transition-none hover:text-foreground focus-visible:ring-0 focus-visible:ring-offset-0 data-[state=active]:border-primary data-[state=active]:text-foreground data-[state=active]:shadow-none"
-                >
-                  {category.name}
-                </TabsTrigger>
-              </Link>
-            ))}
-          </TabsList>
-        </Tabs>
+        <CategoryTabs categories={props.categories} />
       </div>
     </header>
   );
