36c812895ef7f00dfca3a3c8a4e4abeadc0d3240
sacrwist <sacrwist@gmail.com>
2025-04-10 12:09:09 +0900

    ♻️

--- src/features/articles/api/common.ts
+++ src/features/articles/api/common.ts
@@ -1,37 +1,44 @@
+import { ArticleRes } from '@/features/articles/api/response';
 import { client } from '@/lib/microcms';
+import { MicroCMSListResponse } from 'microcms-js-sdk';
 
-export const getArticles = async () => {
+export const getArticles = async (): Promise<ArticleRes[]> => {
   try {
-    const res = await client.get({
+    const res = await client.get<MicroCMSListResponse<ArticleRes>>({
       endpoint: 'articles',
     });
     return res.contents;
   } catch (e) {
     console.error(e);
+    return [];
   }
 };
 
-export const getArticlesWithCategory = async (slug: string) => {
+export const getArticlesWithCategory = async (
+  slug: string
+): Promise<ArticleRes[]> => {
   try {
-    const res = await client.get({
+    const res = await client.get<MicroCMSListResponse<ArticleRes>>({
       endpoint: 'articles',
       queries: { filters: `category[equals]${slug}` },
     });
     return res.contents;
   } catch (e) {
     console.error(e);
+    return [];
   }
 };
 
-export const getArticle = async (slug: string) => {
+export const getArticle = async (slug: string): Promise<ArticleRes | null> => {
   try {
-    const res = await client.get({
+    const res = await client.get<MicroCMSListResponse<ArticleRes>>({
       endpoint: `articles`,
       queries: { filters: `slug[equals]${slug}`, limit: 1 },
     });
     return res.contents[0];
   } catch (e) {
     console.error(e);
+    return null;
   }
 };
 
---
+++ src/features/articles/api/response.ts
@@ -0,0 +1,24 @@
+export interface ArticleRes {
+  id: string;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  content: string;
+  eyecatch?: {
+    url: string;
+    height: number;
+    width: number;
+  };
+  category: {
+    id: string;
+    slug: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    revisedAt: string;
+    name: string;
+  };
+}
